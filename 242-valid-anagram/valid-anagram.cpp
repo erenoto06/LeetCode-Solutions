d7415115c85a7dd54fade11c293944aac89620a0
erenoto06 <erenoto1116@gmail.com>
2026-08-14 14:38:51 +0300

    Time: 4 ms (34.08%) | Memory: 10.1 MB (11.54%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         unordered_map<char, int> letterCount;
-        if (s.size() != t.size()) {
+        if(s.size() != t.size()){
             return false;
         }
         for (int i = 0;i < s.size();i++) {
