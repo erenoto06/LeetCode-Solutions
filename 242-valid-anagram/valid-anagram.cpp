8d339abe15640631426ca51d06b03acbd58fb7b5
erenoto06 <erenoto1116@gmail.com>
2026-08-14 14:38:21 +0300

    Time: 3 ms (45.39%) | Memory: 10 MB (11.54%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> letterCount;
+        if (s.size() != t.size()) {
+            return false;
+        }
+        for (int i = 0;i < s.size();i++) {
+            letterCount[s[i]]++;
+            letterCount[t[i]]--;
+        }
+        for (auto [letter, num] : letterCount) {
+            if (num != 0) {
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
