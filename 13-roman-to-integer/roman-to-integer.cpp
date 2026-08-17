7f0bc01aab7a495fb7d9c721b6032ecd8a683b47
erenoto06 <erenoto1116@gmail.com>
2026-08-17 14:35:53 +0300

    Time: 3 ms (75.83%) | Memory: 9.8 MB (99.99%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
+    int getRomanValue(char c) {
+        switch (c) {
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+        default: return 0;
+        }
+    }
+
     int romanToInt(string s) {
-        unordered_map<char, int> romanValues;
-        romanValues['I'] = 1;
-        romanValues['V'] = 5;
-        romanValues['X'] = 10;
-        romanValues['L'] = 50;
-        romanValues['C'] = 100;
-        romanValues['D'] = 500;
-        romanValues['M'] = 1000;
+       
         int result = 0;
         int index = 0;
         while (index < s.size()) {
             if (index + 1 < s.size()) {
-                if (romanValues[s[index]] >= romanValues[s[index + 1]]) {
-                    result += romanValues[s[index]];
+                if (getRomanValue(s[index]) >= getRomanValue(s[index + 1])) {
+                    result += getRomanValue(s[index]);
                     index++;
                 }
                 else {
-                    result += (romanValues[s[index + 1]] - romanValues[s[index]]);
+                    result += (getRomanValue(s[index + 1]) - getRomanValue(s[index]));
                     index += 2;
                 }
             }
             else {
-                result += romanValues[s[index]];
+                result += getRomanValue(s[index]);
                 index++;
             }
         }
