1234c262c393d042f84e652188e65ba8420837f6
erenoto06 <erenoto1116@gmail.com>
2026-09-14 20:51:51 +0300

    Time: 0 ms (100.00%) | Memory: 65.3 MB (51.68%) - LeetSync

---
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> incoming(n+1,0);
+        vector<int> outgoing(n+1,0);
+        for (int i = 0;i < trust.size();i++) {
+            outgoing[trust[i][0]]++;
+            incoming[trust[i][1]]++;
+
+        }
+        for (int i = 1;i <= n;i++) {
+            if (incoming[i] == n - 1 && outgoing[i] == 0) {
+                return i;
+            }
+
+
+        }
+        return -1;
+        
+    }
+};
