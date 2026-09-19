1e891d47c4b3d90a88c87c99c4c0b348c996502f
erenoto06 <erenoto1116@gmail.com>
2026-09-19 15:33:06 +0300

    Time: 118 ms (42.83%) | Memory: 142.3 MB (7.29%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> index;
+        for (int i = 0;i < nums.size();i++) {
+            if (!index.count(nums[i])) {
+                index[nums[i]] = i;
+            }
+            else {
+                if (i - index[nums[i]] <= k) {
+                    return true;
+                }
+                else {
+                    index[nums[i]] = i;
+                }
+            }
+        }
+        return false;
+        
+        
+    }
+};
