1564bea6744870f4c6add6ad0631016b522ef778
erenoto06 <erenoto1116@gmail.com>
2026-09-20 20:03:33 +0300

    Time: 50 ms (20.86%) | Memory: 85.9 MB (53.39%) - LeetSync

---
+++ 164-maximum-gap/maximum-gap.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int maxGap = 0;
+        for (int i = 0;i < nums.size()-1;i++) {
+            maxGap = max(nums[i + 1] - nums[i], maxGap);
+        }
+        return maxGap;
+    }
+};
